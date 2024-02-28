d815b3f3cd01236230b6c1b2deadd2fd82aec99a
mruizsag <116658771+mruizsag@users.noreply.github.com>
2024-02-28 18:03:54 +0100

    s

--- analysis/6_sliding_window_carbon.R
+++ analysis/6_sliding_window_carbon.R
@@ -1,5 +1,5 @@
 #--------------------------------------------------------------------------------
-#Sliding window analysis with the time-calibrated stable isotope data (nitrogen)
+#Sliding window analysis with the time-calibrated stable isotope data (carbon)
 #--------------------------------------------------------------------------------
 
 # adapted from DR de Zwaan, A Drake, JL Greenwood, and K Martin (2020).
@@ -77,52 +77,52 @@ Bp_isotopic_data_use$sample_ID <- seq.int(nrow(Bp_isotopic_data_use)) #1381
 
 # 10.1. Test if sex should be included to control for among sex differences
 
-wo_sex_model <- lmer(dN ~ poly(julian_day,2) + (1| whale_id), data=Bp_isotopic_data_use)
-sex_model <- lmer(dN ~ poly(julian_day,2) + as.factor(Bp_isotopic_data_use$Sex) + (1| whale_id), data=Bp_isotopic_data_use)
+wo_sex_model <- lmer(d13c.cor ~ poly(julian_day,2) + (1| whale_id), data=Bp_isotopic_data_use)
+sex_model <- lmer(d13c.cor ~ poly(julian_day,2) + as.factor(Bp_isotopic_data_use$Sex) + (1| whale_id), data=Bp_isotopic_data_use)
 
 anova(wo_sex_model, sex_model, test="Chisq") # Decision: Evidence that sex does not improve the model. Do not include.
 
 # 10.2. Test if lenght should be included to control for among lenght differences
 
-wo_lenght_model <- lmer(dN ~ poly(julian_day,2) + (1| whale_id), data=Bp_isotopic_data_use)
-lenght_model <- lmer(dN ~ poly(julian_day,2) + as.factor(Bp_isotopic_data_use$Length) + (1| whale_id), data=Bp_isotopic_data_use)
+wo_lenght_model <- lmer(d13c.cor ~ poly(julian_day,2) + (1| whale_id), data=Bp_isotopic_data_use)
+lenght_model <- lmer(d13c.cor ~ poly(julian_day,2) + as.factor(Bp_isotopic_data_use$Length) + (1| whale_id), data=Bp_isotopic_data_use)
 
-anova(wo_lenght_model, lenght_model, test="Chisq") # Decision: Evidence that lenght does not improve the model. Lenght almost significative.
+anova(wo_lenght_model, lenght_model, test="Chisq") # Decision: Evidence lenght improve the model.
 
 # 10.3. Test if reproductive status should be included to control for among status differences
 
-wo_status_model <- lmer(dN ~ poly(julian_day,2) + (1| whale_id), data=Bp_isotopic_data_use)
-status_model <- lmer(dN ~ poly(julian_day,2) + as.factor(Bp_isotopic_data_use$Status) + (1| whale_id), data=Bp_isotopic_data_use)
+wo_status_model <- lmer(d13c.cor ~ poly(julian_day,2) + (1| whale_id), data=Bp_isotopic_data_use)
+status_model <- lmer(d13c.cor ~ poly(julian_day,2) + as.factor(Bp_isotopic_data_use$Status) + (1| whale_id), data=Bp_isotopic_data_use)
 
 anova(wo_status_model, status_model, test="Chisq") # Decision: Evidence that reproductive status does not improve the model. Do not include.
 
 # 11. Sliding windows
 
-Bp_dN_clim_m_rel <- 
-  climwin::slidingwin(baseline = lme4::lmer(dN ~ 1 + poly(Bp_isotopic_data_use$julian_day,2) + (1| Bp_isotopic_data_use$whale_id), data=Bp_isotopic_data_use),  #this is the base model to which the program adds the climate variables
-  xvar = list(NAO=data_climate$NAO_index,
-              AMO= data_climate$AMO_index,
-              AMOC= data_climate$AMOC_index),
-  type = "relative", 
-  range= c(36,0), 
-  stat = c("mean"),
-  func = c("lin"),
-  cinterval = "month",
-  cmissing = "method2", # only applied in two months of AMOC index
-  cdate = data_climate$date_use, bdate = Bp_isotopic_data_use$measured_date_formatted)
+Bp_dC_clim_m_rel <- 
+  climwin::slidingwin(baseline = lme4::lmer(d13c.cor ~ 1 + poly(Bp_isotopic_data_use$julian_day,2) + (1| Bp_isotopic_data_use$whale_id), data=Bp_isotopic_data_use),  #this is the base model to which the program adds the climate variables
+                      xvar = list(NAO=data_climate$NAO_index,
+                                  AMO= data_climate$AMO_index,
+                                  AMOC= data_climate$AMOC_index),
+                      type = "relative", 
+                      range= c(36,0), 
+                      stat = c("mean"),
+                      func = c("lin"),
+                      cinterval = "month",
+                      cmissing = "method2", # only applied in two months of AMOC index
+                      cdate = data_climate$date_use, bdate = Bp_isotopic_data_use$measured_date_formatted)
 
-Bp_dN_clim_m_rel$combos #check all of the sliding windows in the analyses
+Bp_dC_clim_m_rel$combos #check all of the sliding windows in the analyses
 
 #AIC difference < 2
-a<-head(Bp_dN_clim_m_rel[[1]]$Dataset, 20) ## Lin_func Mean NAO, close window-open window (34-31) (beta coefficient= -0.36)
+a<-head(Bp_dC_clim_m_rel[[1]]$Dataset, 20) ## Lin_func Mean NAO, close window-open window (24-6) (beta coefficient= 0.16)
 a <- a %>% filter(deltaAICc < (a$deltaAICc[1])+2)
 nrow_a <- nrow(a)
 a$Index <- c(rep("NAO", nrow_a))
-b<-head(Bp_dN_clim_m_rel[[2]]$Dataset, 20) ## Lin_func Mean AMO, close window-open window (32-27) (beta coefficient= 2.57)
+b<-head(Bp_dC_clim_m_rel[[2]]$Dataset, 20) ## Lin_func Mean AMO, close window-open window (10-9) (beta coefficient= -0.54)
 b <- b %>% filter(deltaAICc < (b$deltaAICc[1])+2)
 nrow_b <- nrow(b)
 b$Index <- c(rep("AMO", nrow_b))
-c<-head(Bp_dN_clim_m_rel[[3]]$Dataset, 20) ## Lin_func Mean AMOC, close window-open window (30-13) (beta coefficient= 0.35)
+c<-head(Bp_dC_clim_m_rel[[3]]$Dataset, 20) ## Lin_func Mean AMOC, close window-open window (33-23) (beta coefficient= -0.03)
 c <- c %>% filter(deltaAICc < (c$deltaAICc[1])+2)
 nrow_c <- nrow(c)
 c$Index <- c(rep("AMOC", nrow_c))
@@ -145,20 +145,20 @@ ggplot(data) +
 
 
 # 12.Randomization
-Bp_dN_rand_clim_m_rel <- randwin(repeats = 1000, 
-                                baseline = lme4::lmer(dN ~ 1 + poly(Bp_isotopic_data_use$julian_day,2) + (1| Bp_isotopic_data_use$whale_id), data=Bp_isotopic_data_use),  #this is the base model to which the program adds the climate variables
-                                xvar = list(NAO=data_climate$NAO_index,
-                                            AMO= data_climate$AMO_index,
-                                            AMOC= data_climate$AMOC_index),
-                                type = "relative", 
-                                range= c(36,0), 
-                                stat = c("mean"),
-                                func = c("lin"),
-                                cinterval = "month",
-                                cmissing = "method2", # only applied in two months of AMOC index
-                                cdate = data_climate$date_use, bdate = Bp_isotopic_data_use$measured_date_formatted)
+Bp_dC_rand_clim_m_rel <- randwin(repeats = 1000, 
+                                 baseline = lme4::lmer(d13c.cor ~ 1 + poly(Bp_isotopic_data_use$julian_day,2) + (1| Bp_isotopic_data_use$whale_id), data=Bp_isotopic_data_use),  #this is the base model to which the program adds the climate variables
+                                 xvar = list(NAO=data_climate$NAO_index,
+                                             AMO= data_climate$AMO_index,
+                                             AMOC= data_climate$AMOC_index),
+                                 type = "relative", 
+                                 range= c(36,0), 
+                                 stat = c("mean"),
+                                 func = c("lin"),
+                                 cinterval = "month",
+                                 cmissing = "method2", # only applied in two months of AMOC index
+                                 cdate = data_climate$date_use, bdate = Bp_isotopic_data_use$measured_date_formatted)
 
-Bp_dN_rand_clim_m_rel
+Bp_dC_rand_clim_m_rel
 
 
 
@@ -166,15 +166,15 @@ Bp_dN_rand_clim_m_rel
 
 
 # Mean NAO
-pvalue(dataset = Bp_dN_clim_m_rel[[1]]$Dataset, datasetrand = Bp_dN_rand_clim_m_rel[[1]], 
+pvalue(dataset = Bp_dC_clim_m_rel[[1]]$Dataset, datasetrand = Bp_dC_rand_clim_m_rel[[1]], 
        metric="AIC", sample.size=12) # "<0.001"
 
 # Mean AMO
-pvalue(dataset = Bp_dN_clim_m_rel[[2]]$Dataset, datasetrand = Bp_dN_rand_clim_m_rel[[2]], 
+pvalue(dataset = Bp_dC_clim_m_rel[[2]]$Dataset, datasetrand = Bp_dC_rand_clim_m_rel[[2]], 
        metric="AIC", sample.size=12) # "<0.001"
 
 # Mean AMOC
-pvalue(dataset = Bp_dN_clim_m_rel[[3]]$Dataset, datasetrand = Bp_dN_rand_clim_m_rel[[3]], 
+pvalue(dataset = Bp_dC_clim_m_rel[[3]]$Dataset, datasetrand = Bp_dC_rand_clim_m_rel[[3]], 
        metric="AIC", sample.size=12) # "<0.001"
 
 
@@ -182,18 +182,18 @@ pvalue(dataset = Bp_dN_clim_m_rel[[3]]$Dataset, datasetrand = Bp_dN_rand_clim_m_
 
 # NAO index
 #Best window for mean NAO linear 34-31
-Bp_isotopic_data_use$mean_NAO_linear_start_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(34)
-Bp_isotopic_data_use$mean_NAO_linear_end_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(31)
+Bp_isotopic_data_use$mean_NAO_linear_start_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(24)
+Bp_isotopic_data_use$mean_NAO_linear_end_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(6)
 
 # AMO index
 #Best window for mean AO linear 32-27
-Bp_isotopic_data_use$mean_AMO_linear_start_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(32)
-Bp_isotopic_data_use$mean_AMO_linear_end_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(27)
+Bp_isotopic_data_use$mean_AMO_linear_start_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(10)
+Bp_isotopic_data_use$mean_AMO_linear_end_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(9)
 
 # AMOC
 #Best window for mean AMOC linear 30-14
-Bp_isotopic_data_use$mean_AMOC_linear_start_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(30)
-Bp_isotopic_data_use$mean_AMOC_linear_end_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(14)
+Bp_isotopic_data_use$mean_AMOC_linear_start_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(33)
+Bp_isotopic_data_use$mean_AMOC_linear_end_m <- ymd(Bp_isotopic_data_use$measured)%m-% months(23)
 
 # Select entire 36-month period for all climate variables for summary table
 Bp_isotopic_data_use$overall_start <- ymd(Bp_isotopic_data_use$measured)%m-% months(36)
@@ -244,13 +244,13 @@ Bp_isotopic_data_use$overall_end_date <- as.POSIXct(Bp_isotopic_data_use$overall
 
 # Empty lists
 
-mean_NAO_dN_list <- list()
+mean_NAO_dC_list <- list()
 
 
-mean_AMO_dN_list <- list()
+mean_AMO_dC_list <- list()
 
 
-mean_AMOC_dN_list <- list()
+mean_AMOC_dC_list <- list()
 
 
 overall_list <- list()
@@ -258,27 +258,27 @@ overall_list <- list()
 
 for (i in 1:1381) { # loop from one to number of samples samples 
   # Average NAO index
-  mean_NAO_dN_list[[i]] <- data_climate %>% 
+  mean_NAO_dC_list[[i]] <- data_climate %>% 
     filter(date_new >= Bp_isotopic_data_use$mean_NAO_start_m_date[i] 
            & date_new <= Bp_isotopic_data_use$mean_NAO_end_m_date[i]) %>% 
     
-    summarise(mean_NAO_dN = mean(NAO_index))
+    summarise(mean_NAO_dC = mean(NAO_index))
   
   
   # Average AMO index
-  mean_AMO_dN_list[[i]] <- data_climate %>% 
+  mean_AMO_dC_list[[i]] <- data_climate %>% 
     filter(date_new >= Bp_isotopic_data_use$mean_AMO_start_m_date[i] 
            & date_new <= Bp_isotopic_data_use$mean_AMO_end_m_date[i]) %>% 
     
-    summarise(mean_AMO_dN = mean(AMO_index))
+    summarise(mean_AMO_dC = mean(AMO_index))
   
   
   # Average AMOC index
-  mean_AMOC_dN_list[[i]] <- data_climate %>% 
+  mean_AMOC_dC_list[[i]] <- data_climate %>% 
     filter(date_new >= Bp_isotopic_data_use$mean_AMOC_start_m_date[i] 
            & date_new <= Bp_isotopic_data_use$mean_AMOC_end_m_date[i]) %>% 
     
-    summarise(mean_AMOC_dN = mean(AMOC_index))
+    summarise(mean_AMOC_dC = mean(AMOC_index))
   
   # Overall
   overall_list[[i]] <- data_climate %>% 
@@ -292,24 +292,24 @@ for (i in 1:1381) { # loop from one to number of samples samples
   
   # Convert lists to rows per sample ID
   
-  mean_NAO_dN_data <- bind_rows(mean_NAO_dN_list)
+  mean_NAO_dC_data <- bind_rows(mean_NAO_dC_list)
   
-  mean_AMO_dN_data <- bind_rows(mean_AMO_dN_list)
+  mean_AMO_dC_data <- bind_rows(mean_AMO_dC_list)
   
-  mean_AMOC_dN_data <- bind_rows(mean_AMOC_dN_list)
+  mean_AMOC_dC_data <- bind_rows(mean_AMOC_dC_list)
   
   overall_data <- bind_rows(overall_list)
   
   
   # Convert each variable to a column in a larger dataset
-  Bp_dN_windows <- bind_cols(mean_NAO_dN_data,
-                             mean_AMO_dN_data, 
-                             mean_AMOC_dN_data,
+  Bp_dC_windows <- bind_cols(mean_NAO_dC_data,
+                             mean_AMO_dC_data, 
+                             mean_AMOC_dC_data,
                              overall_data)
   
 }
 
-head(Bp_dN_windows)
+head(Bp_dC_windows)
 
 
 ### Combine with nestquadg measurement data
@@ -318,10 +318,10 @@ head(Bp_isotopic_data_use)
 
 Bp_isotopic_data_new <- Bp_isotopic_data_use # Extract only columns of interest from original data
 
-Bp_dN_final <- dplyr::bind_cols(Bp_isotopic_data_new, Bp_dN_windows)
+Bp_dC_final <- dplyr::bind_cols(Bp_isotopic_data_new, Bp_dC_windows)
 
 
 ### Save data
 
-write.csv(Bp_dN_final, "/Users/marcruizisagales/Documents/GitHub/Climate-baleen-plate-isotopes/data/Bp_isotopic_climate_summarised_final_nitrogen_mean_seasonality_for_Github.csv")
+write.csv(Bp_dC_final, "/Users/marcruizisagales/Documents/GitHub/Climate-baleen-plate-isotopes/data/Bp_isotopic_climate_summarised_final_carbon_mean_seasonality_for_Github.csv")
 
