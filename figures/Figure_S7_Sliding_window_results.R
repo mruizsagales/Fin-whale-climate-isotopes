12c6da3ee184b9b37c376c2c4563956b8d73abd2
mruizsag <116658771+mruizsag@users.noreply.github.com>
2024-02-28 18:50:58 +0100

    Create Figure_S7_Sliding_window_results.R

---
+++ figures/Figure_S7_Sliding_window_results.R
@@ -0,0 +1,112 @@
+#--------------------------------------------------------------------------------
+#Figure S7 - Sliding window results
+#--------------------------------------------------------------------------------
+
+# adapted from DR de Zwaan, A Drake, JL Greenwood, and K Martin (2020).
+# readapted by Marc Ruiz-Sagalés May 2022.
+
+#--------------------------------------------------------------------------------
+
+# 1. Load libraries
+library(lubridate)
+library(plyr)
+library(dplyr)
+library(lme4)
+library(climwin)
+library(readxl)
+library(readr)
+library(ggplot2)
+library(tidyverse)
+library(plyr)
+library(mgcv)
+library(lmerTest)
+library(car)
+library(nortest)
+library(effects)
+library(nlme)
+library(lvmisc)
+library(egg)
+library(RColorBrewer)
+library(ggtext)
+library(ggeffects)
+library(patchwork)
+
+# 2. Load dependencies
+source("/Users/marcruizisagales/Documents/GitHub/Climate-baleen-plate-isotopes/analysis/4_sliding_window_nitrogen.R")
+source("/Users/marcruizisagales/Documents/GitHub/Climate-baleen-plate-isotopes/analysis/6_sliding_window_carbon.R")
+
+# 3. Sliding window results
+set.seed(123)
+
+# 4. Plot sliding window results
+
+# nitrogen
+
+a<-head(Bp_dN_clim_m_rel[[1]]$Dataset, 20) ## Lin_func Mean NAO, close window-open window (34-31) (beta coefficient= -0.3619122)
+a <- a %>% filter(deltaAICc < (a$deltaAICc[1])+2)
+nrow_a <- nrow(a)
+a$Index <- c(rep("NAO", nrow_a))
+b<-head(Bp_dN_clim_m_rel[[2]]$Dataset, 20) ## Lin_func Mean AMO, close window-open window (52-35) (beta coefficient= -4.377916)
+b <- b %>% filter(deltaAICc < (a$deltaAICc[1])+2)
+nrow_b <- nrow(b)
+b$Index <- c(rep("AMO", nrow_b))
+
+data <- rbind(a,b)
+
+levels=c("NAO","AMO")
+
+data$Index <- factor(data$Index, levels=rev(c("NAO","AMO")))
+
+m1 <- ggplot(data) +
+  geom_vline(aes(xintercept = data$Index), color = "gray", linetype = "dotted") +
+  geom_linerange(aes(x = data$Index, ymin = WindowOpen, ymax = WindowClose), color = '#74A9CF', size = 20, alpha = 0.9) +  # Afegeix les línies de separació
+  geom_hline(yintercept = seq(1,48,1), lty = 1, lwd = 0.1, colour = "white") +
+  geom_hline(yintercept = 0, lty = 1, lwd = 1, colour = "black") +
+  coord_flip() +
+  labs(x = "", y = "Months from sample", title = "") +
+  theme_article(base_size = 20) + ylim(36,0)+
+  scale_x_discrete(breaks=levels,labels=c("Avg NAO index","Avg AMO index")) + theme(aspect.ratio=3/4)
+
+m1
+
+# carbon
+
+a1<-head(Bp_dC_clim_m_rel[[2]]$Dataset, 20) ## Lin_func Mean NAO, close window-open window (34-31) (beta coefficient= -0.3619122)
+a1 <- a1 %>% filter(deltaAICc < (a1$deltaAICc[1])+2)
+nrow_a1 <- nrow(a1)
+a1$Index <- c(rep("AMO", nrow_a1))
+
+data1 <- rbind(a1)
+
+levels1=c("AMO")
+
+data1$Index <- factor(data1$Index, levels=c(levels1))
+
+m2 <- ggplot(data1) +
+  geom_vline(aes(xintercept = data1$Index), color = "gray", linetype = "dotted") +
+  geom_linerange(aes(x = data1$Index, ymin = WindowOpen, ymax = WindowClose), color = '#FC8D59', size = 20, alpha =0.9) +  # Afegeix les línies de separació
+  geom_hline(yintercept = seq(1,48,1), lty = 1, lwd = 0.1, colour = "white") +
+  geom_hline(yintercept = 0, lty = 1, lwd = 1, colour = "black") +
+  coord_flip() +
+  labs(x = "", y = "Months from sample", title = "") +
+  theme_article(base_size = 20) + ylim(36,0)+
+  scale_x_discrete(breaks=levels1,labels=c("Avg AMO index")) + theme(aspect.ratio=3/4)
+
+m1+m2
+
+# Toghether 
+
+m3 <- ggplot(data) +
+  geom_vline(aes(xintercept = data$Index), color = "gray", linetype = "dotted") +
+  geom_linerange(aes(x = data$Index, ymin = WindowOpen, ymax = WindowClose), color = '#74A9CF', size = 20, alpha = 0.9) +  # Afegeix les línies de separació
+  geom_hline(yintercept = seq(1,48,1), lty = 1, lwd = 0.1, colour = "white") +
+  geom_hline(yintercept = 0, lty = 1, lwd = 1, colour = "black") +
+  geom_linerange(data=data1, aes(x = Index, ymin = WindowOpen, ymax = WindowClose), color = '#FC8D59', size = 20, alpha =0.9) +  # Afegeix les línies de separació
+  coord_flip() +
+  labs(x = "", y = "Time from last keratin sample (months)", title = "") +
+  theme_article(base_size = 20) + ylim(36,0)+
+  scale_x_discrete(breaks=levels,labels=c("Avg NAO index","Avg AMO index")) + theme(aspect.ratio=3/4)
+m3
+
+ggsave("/Users/marcruizisagales/Documents/GitHub/Climate-baleen-plate-isotopes/png/Figure_S7_Sliding_window_results.png", m3, 
+       device = png(width = 1200, height = 450))
